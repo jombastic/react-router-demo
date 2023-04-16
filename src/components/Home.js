6070cf8e47daa3427b55e2c6c71e8ac1148bdf53
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-04-16 12:34:11 +0200

    navigate programaticaly through pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const navigate = useNavigate();
+    return (
+        <>
+            <div>Home Page</div>
+            <button onClick={() => navigate('order-summary', { replace: true })}>Place order</button>
+        </>
+    )
 }
 
 export default Home
