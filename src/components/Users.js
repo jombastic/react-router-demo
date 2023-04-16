e3e7a49214a135ff745fe1b6dd068f52d1efdf16
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-04-16 20:56:12 +0200

    set search params via buttons in Users components and display content based on search params

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useSearchParams } from 'react-router-dom'
 
 const Users = () => {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const showActiveUsers = searchParams.get('filter') === 'active'
+
     return (
         <div>
             <h2>User 1</h2>
             <h2>User 2</h2>
             <h2>User 3</h2>
             <Outlet />
-        </div>
+            <div>
+                <button onClick={() => setSearchParams({ filter: 'active' })}>Active Users</button>
+                <button onClick={() => setSearchParams({})}>Reset Users</button>
+                {
+                    showActiveUsers ? <h2>Showing active usesrs</h2> : <h2>Showing all users</h2>
+                }
+            </div>
+        </div >
     )
 }
 
