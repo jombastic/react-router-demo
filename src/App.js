ee040146b6c6482e6f875b5104046a1bcaa882ea
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-04-16 13:34:11 +0200

    create dynamic routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import NoMatch from './components/NoMatch';
 import Products from './components/Products';
 import FeaturedProducts from './components/FeaturedProducts';
 import NewProducts from './components/NewProducts';
+import Users from './components/Users';
+import UserDetails from './components/UserDetails';
+import Admin from './components/Admin';
 
 function App() {
   return (
@@ -21,7 +24,11 @@ function App() {
           <Route path='featured' element={<FeaturedProducts />} />
           <Route path='new' element={<NewProducts />} />
         </Route>
-        <Route path='*' element={<NoMatch />} />
+        <Route path='users' element={<Users />}>
+          <Route path=':userId' element={<UserDetails />} />
+          <Route path='admin' element={<Admin />} />
+          <Route path='*' element={<NoMatch />} />
+        </Route>
       </Routes>
     </>
   );
