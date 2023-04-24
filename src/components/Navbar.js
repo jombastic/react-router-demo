10bf08717a5cb0569128c4bde78c504d61fc0c6f
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-04-24 13:11:00 +0200

    setup an authentication for some routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useAuth } from './auth'
 
 const Navbar = () => {
     const navLinkStyles = ({ isActive }) => {
@@ -9,11 +10,17 @@ const Navbar = () => {
         }
     }
 
+    const auth = useAuth()
+
     return (
         <nav className='primary-nav'>
             <NavLink style={navLinkStyles} to='/'>Home</NavLink>
             <NavLink style={navLinkStyles} to='/about'>About</NavLink>
             <NavLink style={navLinkStyles} to='/products'>Products</NavLink>
+            <NavLink style={navLinkStyles} to='/profile'>Profile</NavLink>
+            {!auth.user && (
+                <NavLink style={navLinkStyles} to='/login'>Login</NavLink>
+            )}
         </nav>
     )
 }
